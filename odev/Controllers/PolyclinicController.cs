05646b2defea969372f26e19a44322569dda70b6
Beyza Çağlı <beyza.cagli@ogr.sakarya.edu.tr>
2023-12-25 23:52:08 +0300

    poliklinik controller'ı oluşturuldu.

---
+++ odev/Controllers/PolyclinicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace odev.Controllers
+{
+    public class PolyclinicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
