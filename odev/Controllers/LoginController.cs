34974797d98710b7f1f8af0ddaf48a363a752f05
Beyza Çağlı <beyza.cagli@ogr.sakarya.edu.tr>
2023-12-25 23:44:49 +0300

    login controller'ı oluşturuldu.

---
+++ odev/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace odev.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
