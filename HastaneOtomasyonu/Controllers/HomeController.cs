cb465a0ddb0ba01a99fc5ddc80021d43fe3d7d9f
Beyza Çağlı <beyza.cagli@ogr.sakarya.edu.tr>
2023-12-30 20:30:50 +0300

    iletişim sayfası yapıldı.

--- HastaneOtomasyonu/Controllers/HomeController.cs
+++ HastaneOtomasyonu/Controllers/HomeController.cs
@@ -23,6 +23,13 @@ namespace HastaneOtomasyonu.Controllers
             return View();
         }
 
+        //
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+        //
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
